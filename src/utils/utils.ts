72fe8c9e7629b7545048c8c34db4d5cd2bcf3940
nth090801 <thanhhoai160518@gmail.com>
2023-11-18 13:06:16 +0700

    Handle message errors simply with axios interceptor

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,4 @@
-import axios, { AxiosError } from 'axios'
-import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+import axios, { AxiosError, HttpStatusCode } from 'axios'
 
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
   // eslint-disable-next-line import/no-named-as-default-member
