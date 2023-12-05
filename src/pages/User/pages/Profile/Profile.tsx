5ae51342290ed29a1d23c6ef074eda89d1c1b0b0
nth090801 <thanhhoai160518@gmail.com>
2023-12-05 15:00:36 +0700

    Logic code to change password

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import userApi from 'src/apis/user.api'
 import Button from 'src/components/Button'
@@ -13,7 +13,7 @@ import { AppContext } from 'src/contexts/app.context'
 import { setProfileToLS } from 'src/utils/auth'
 import { getAvatarUrl, isAxiosUnprocessableEntityError } from 'src/utils/utils'
 import { ErrorResponse } from 'src/types/utils.type'
-import config from 'src/constants/config'
+import InputFile from 'src/components/InputFile'
 
 type FormData = Pick<UserSchema, 'name' | 'address' | 'phone' | 'date_of_birth' | 'avatar'>
 type FormDataError = Omit<FormData, 'date_of_birth'> & {
@@ -23,7 +23,6 @@ type FormDataError = Omit<FormData, 'date_of_birth'> & {
 const profileSchema = userSchema.pick(['name', 'address', 'phone', 'date_of_birth', 'avatar'])
 
 export default function Profile() {
-  const fileInputRef = useRef<HTMLInputElement>(null)
   const [file, setFile] = useState<File>()
   const { setProfile } = useContext(AppContext)
   const previewImage = useMemo(() => {
@@ -103,24 +102,8 @@ export default function Profile() {
       }
     }
   })
-
-  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const fileFromLocal = event.target.files?.[0]
-    fileInputRef.current?.setAttribute('value', '')
-    if (fileFromLocal && (fileFromLocal.size >= config.maxSizeUploadAvatar || !fileFromLocal.type.includes('image'))) {
-      toast.error(
-        `Dụng lượng file tối đa 1 MB
-      Định dạng:.JPEG, .PNG`,
-        {
-          position: 'top-center'
-        }
-      )
-    } else {
-      setFile(fileFromLocal)
-    }
-  }
-  const handleUpload = () => {
-    fileInputRef.current?.click()
+  const handleChangeFile = (file?: File) => {
+    setFile(file)
   }
   return (
     <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
@@ -206,24 +189,7 @@ export default function Profile() {
                 className='h-full w-full rounded-full object-cover'
               />
             </div>
-            <input
-              className='hidden'
-              type='file'
-              accept='.jpg,.jpeg,.png'
-              ref={fileInputRef}
-              onChange={onFileChange}
-              onClick={(event) => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                ;(event.target as any).value = null
-              }}
-            />
-            <button
-              type='button'
-              onClick={handleUpload}
-              className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'
-            >
-              Chọn ảnh
-            </button>
+            <InputFile onChange={handleChangeFile} />
             <div className='mt-3 text-gray-400'>
               <div>Dụng lượng file tối đa 1 MB</div>
               <div>Định dạng:.JPEG, .PNG</div>
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -7,6 +7,14 @@ function testPriceMinMax(this: yup.TestContext<yup.AnyObject>) {
   }
   return price_min !== '' || price_max !== ''
 }
+const handleConfirmPasswordYup = (refString: string) => {
+  return yup
+    .string()
+    .required('Confirm Password là bắt buộc')
+    .min(6, 'Độ dài từ 6 - 160 kí tự')
+    .max(160, 'Độ dài từ 6 - 160 kí tự')
+    .oneOf([yup.ref(refString)], 'Confirm Password không đúng')
+}
 
 export const schema = yup.object({
   email: yup
@@ -20,12 +28,7 @@ export const schema = yup.object({
     .required('Password là bắt buộc')
     .min(6, 'Độ dài từ 6 - 160 kí tự')
     .max(160, 'Độ dài từ 6 - 160 kí tự'),
-  confirm_password: yup
-    .string()
-    .required('Confirm Password là bắt buộc')
-    .min(6, 'Độ dài từ 6 - 160 kí tự')
-    .max(160, 'Độ dài từ 6 - 160 kí tự')
-    .oneOf([yup.ref('password')], 'Confirm Password không đúng'),
+  confirm_password: handleConfirmPasswordYup('password'),
   price_min: yup.string().test({
     name: 'price-not-allowed',
     message: 'Giá không phù hợp',
@@ -45,9 +48,9 @@ export const userSchema = yup.object({
   address: yup.string().max(160, 'Độ dài tối đa là 160 kí tự'),
   avatar: yup.string().max(1000, 'Độ dài tối đa là 1000 kí tự'),
   date_of_birth: yup.date().max(new Date(), 'Hãy chọn một ngày trong quá khứ'),
-  password: schema.fields['password'],
-  new_password: schema.fields['password'],
-  confirm_password: schema.fields['confirm_password']
+  password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  new_password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  confirm_password: handleConfirmPasswordYup('new_password')
 })
 
 export type UserSchema = yup.InferType<typeof userSchema>
